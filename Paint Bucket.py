50ecb6c480f7f02522fd264c17fa8308be13c095
Issa Zananiri <Issa@jtmt.gov.il>
2023-11-11 11:23:52 +0200

    Another Example of Bucket - Instantiation - Inheritence

---
+++ Paint Bucket.py
@@ -0,0 +1,18 @@
+class Paint:
+
+    def __init__(self, buckets, color): # Or get house area and height
+        self.color = color
+        self.buckets = buckets
+
+    def total_price(self):
+        if self.color == "white":
+            return self.buckets * 1.99
+        else:
+            return self.buckets * 2.19
+            
+class DiscountedPaint(Paint):
+    def discounted_price(self, discount_percentage=0.15):
+        return (self.total_price() * discount_percentage)
+
+total_price = DiscountedPaint(4,"white")
+print(total_price.discounted_price(0.8))
